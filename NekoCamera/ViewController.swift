124f0319c93fe6a77bc7aeb2975de768617c2f86
flexsche-higuchi <katsuo1588@gmail.com>
2016-07-29 15:30:52 +0900

    Initial Commit

---
+++ NekoCamera/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  NekoCamera
+//
+//  Created by 樋口陽介 on 2016/07/29.
+//  Copyright © 2016年 樋口陽介. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
